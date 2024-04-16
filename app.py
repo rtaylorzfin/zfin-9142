d133f35799a4c2a646aad7b231863d2667c319b4
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-04-16 14:13:08 -0700

    fix xlsx

--- app.py
+++ app.py
@@ -36,13 +36,30 @@ class ReportRunner:
 
     def combine_all_csvs_into_one_xlsx(self):
         print("Combining all csvs into one xlsx: out/all.xlsx")
+
         if os.path.exists("out/all.xlsx"):
             os.remove("out/all.xlsx")
 
+        #First create the xlsx file
+        with pd.ExcelWriter("out/all.xlsx") as writer:
+            #add worksheet called default
+            pd.DataFrame().to_excel(writer, sheet_name='default', index=False)
+
+        #append all reports
         for report in self.report_files:
             with pd.ExcelWriter("out/all.xlsx", mode='a') as writer:
                 df = pd.read_csv(f"out/{report}.csv")
                 df.to_excel(writer, sheet_name=report, index=False)
+
+                # Auto-adjust columns' width
+                for column in df:
+                    column_length = max(df[column].astype(str).map(len).max(), len(column))
+                    col_idx = df.columns.get_loc(column)
+                    writer.sheets[report].column_dimensions[writer.sheets[report].cell(row=1, column=col_idx + 1).column_letter].width = column_length
+
+        #remove 'default' sheet
+        with pd.ExcelWriter("out/all.xlsx", mode='a') as writer:
+            writer.book.remove(writer.book['default'])
 
     def gene_accession_pairs_lost_query(self):
         query = """
@@ -63,7 +80,9 @@ class ReportRunner:
     def gene_accession_attribs_kept_query(self):
         query = """
                 select distinct dblink_linked_recid as gene, dblink_acc_num as acc, recattrib_source_zdb_id as pub, acc_type from genbank0306
-                where (dblink_linked_recid, dblink_acc_num, recattrib_source_zdb_id) in
+                where recattrib_source_zdb_id in ('ZDB-PUB-020723-3', 'ZDB-PUB-130725-2')
+                AND 
+                (dblink_linked_recid, dblink_acc_num, recattrib_source_zdb_id) in
                 (select distinct dblink_linked_recid, dblink_acc_num, recattrib_source_zdb_id from genbank0408);
         """
         return query
